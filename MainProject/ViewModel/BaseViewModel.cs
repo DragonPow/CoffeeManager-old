e82f28d396119e4094da6a09f251c852cfe6f19c
UIT.19520266 <19520266@gm.uit.edu.vn>
2021-03-29 12:06:31 +0700

    build base class MVVM

---
+++ MainProject/ViewModel/BaseViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainProject.ViewModel
+{
+    abstract class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            handler?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+/*        event PropertyChangedEventHandler INotifyPropertyChanged.PropertyChanged
+        {
+            add
+            {
+                throw new NotImplementedException();
+            }
+
+            remove
+            {
+                throw new NotImplementedException();
+            }
+        }*/
+    }
+}
---
+++ MainProject/ViewModel/RelayingCommand.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MainProject.ViewModel
+{
+    class RelayingCommand : ICommand
+    {
+        #region Fields
+        readonly Predicate<object> _canExecute;
+        readonly Action<object> _execute;
+        #endregion //Fields
+
+
+
+        #region Constructors
+        public RelayingCommand(Action<object> action) : this(action, null) { }
+        public RelayingCommand(Action<object> execute, Predicate<object> canExecute)
+        {
+            if (execute ==null)
+            {
+                throw new NullReferenceException("The Execute is not null");
+            }
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+        #endregion //Constructors
+
+
+
+        #region ICommand Members
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested += value; }
+        }
+        
+        public bool CanExecute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Execute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion //ICommand Members
+    }
+}
