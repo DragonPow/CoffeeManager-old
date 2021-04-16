cab7ed10d592b7013b135657246b8aa4e51e04d1
UIT.19520296 <68140820+thuytien192@users.noreply.github.com>
2021-04-16 17:36:57 +0700

    Produce UI

---
+++ MainProject/MainWorkSpace/Menu/AddProduce_UserControl.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MainProject.MainWorkSpace.Menu
+{
+    /// <summary>
+    /// Interaction logic for AddProduce_UserControl.xaml
+    /// </summary>
+    public partial class AddProduce_UserControl : UserControl
+    {
+        public AddProduce_UserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
