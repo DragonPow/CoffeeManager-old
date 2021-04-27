929baf27d8125a91a4a34c65e801c6dd530431f2
UIT.19521973 <67250352+Nhuc-Huynh@users.noreply.github.com>
2021-04-27 16:18:47 +0700

    BillViewModel

---
+++ MainProject/Store.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainProject
+{
+    public static class Store
+    {
+        #region Field
+        static string _name;
+        static string _phone;
+        static string _address;
+        static string _nameInnkeeper;
+        #endregion
+
+        #region Property
+
+        public static string Name { get => _name; set => _name = value; }
+        public static string Phone { get => _phone; set => _phone = value; }
+        public static string Address { get => _address; set => _address = value; }
+        public static string NameInnkeeper { get => _nameInnkeeper; set => _nameInnkeeper = value; }
+
+        #endregion
+    }
+}
