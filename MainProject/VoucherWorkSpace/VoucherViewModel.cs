634c25792e7c865985d9c92bb7e602cc1d5ecf70
Dragnell <bang2001vl@outlook.com.vn>
2021-05-11 18:56:13 +0700

    add constraint to input

--- MainProject/VoucherWorkSpace/VoucherViewModel.cs
+++ MainProject/VoucherWorkSpace/VoucherViewModel.cs
@@ -1,4 +1,5 @@
-﻿using MaterialDesignThemes.Wpf;
+﻿using MainProject.Model;
+using MaterialDesignThemes.Wpf;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,6 +13,7 @@ namespace MainProject.VoucherWorkSpace
     {
         public string NameWorkSpace => "Voucher";
         private const PackIconKind _IconDisplay = PackIconKind.GiftOutline;
+
         public PackIcon IconDisplay
         {
             get
@@ -20,8 +22,63 @@ namespace MainProject.VoucherWorkSpace
             }
         }
 
-        public DateTime DateStart = new DateTime(2001, 1,1, 0,0,0);
-        public DateTime DateEnd = new DateTime(2001, 1, 1,23,59,59);
+        private DateTime dateStart;
+        public DateTime DateStart_Date { get => DateStart.Date;
+            set
+            {
+                if (DateStart.Date != value)
+                {
+                    this.DateStart = new DateTime(DateStart.TimeOfDay.Ticks + value.Ticks);
+                    OnPropertyChanged("DateStart_Date");
+                }
+            }
+        }
+        public TimeSpan DateStart_Time { get => DateStart.TimeOfDay;
+            set
+            {
+                if (DateStart.TimeOfDay != value)
+                {
+                    this.DateStart = new DateTime(DateStart.Date.Ticks + value.Ticks);
+                    OnPropertyChanged("DateStart_Time");
+                }
+            }
+        }
+
+        private DateTime dateEnd;
+        public DateTime DateEnd { get => dateEnd;
+            set
+            {
+                if (this.dateEnd != value)
+                {
+                    this.dateEnd = value;
+                    OnPropertyChanged("DateEnd");
+                }
+            }
+        }
+        public DateTime DateEnd_Date
+        {
+            get => dateEnd.Date;
+            set
+            {
+                if (this.dateEnd.Date != value)
+                {
+                    this.dateEnd = new DateTime(value.Ticks + dateEnd.TimeOfDay.Ticks);
+                    OnPropertyChanged("DateEnd_Date");
+                }
+            }
+        }
+        public TimeSpan DateEnd_Time
+        {
+            get => dateEnd.TimeOfDay;
+            set
+            {
+                if (this.dateEnd.TimeOfDay != value)
+                {
+                    this.dateEnd = new DateTime(value.Ticks + dateEnd.Date.Ticks);
+                    OnPropertyChanged("DateEnd_Time");
+                }
+            }
+        }
 
         private int _value = 20;
         public String Value
@@ -29,9 +86,15 @@ namespace MainProject.VoucherWorkSpace
             get => _value.ToString();
             set
             {
-                if (int.TryParse(value, out _value))
+                int input = -1;
+                try { input = int.Parse(value); }
+                catch (Exception) { }
+
+                if (input > 0 && input != _value)
                 {
+                    _value = input;
                     OnPropertyChanged("ValueString");
+                    OnPropertyChanged("Value");
                 }
             }
         }
@@ -69,6 +132,95 @@ namespace MainProject.VoucherWorkSpace
             }
         }
 
-        
+        public bool IsAuto { get => isAuto; set
+            {
+                if (isAuto != value)
+                {
+                    isAuto = value;
+                    OnPropertyChanged("IsAuto");
+                }
+            }
+        }
+
+        public DateTime DateStart
+        {
+            get => dateStart;
+            set
+            {
+                if (this.dateStart != value)
+                {
+                    dateStart = value;
+                    OnPropertyChanged("DateStart");
+                }
+            }
+        }
+
+        private bool isAuto;
+
+        public VoucherViewModel()
+        {
+
+            this.Code = "";
+            this.Description = "Empty description";
+            this.Value = "20";
+
+            this.dateStart = DateTime.Now;
+            this.dateEnd = DateTime.Now.AddDays(1);
+        }
+
+        public String getRandomCode() 
+            {
+            const String chars = "QWERTYUIOPASDFGHJKLZXCVBNM0123456789";
+            Random random = new Random(DateTime.Now.TimeOfDay.Milliseconds);
+            StringBuilder builder = new StringBuilder(16);
+            for (int i = 0; i < builder.Capacity; i++)
+            {
+                builder.Append(chars.ElementAt(random.Next(chars.Length)));
+            }
+            return builder.ToString();
+        }
+
+        public void RandomCode()
+        {
+            using (mainEntities db = new mainEntities())
+            {
+                /*VOUCHER voucher = db.VOUCHERs.Where((v) => v.ID == this._code).First();
+                while (voucher != null)
+                {
+                    _code = getRandomCode();
+                    voucher = db.VOUCHERs.Where((v) => v.ID == _code).First();
+                }*/
+            }
+            OnPropertyChanged("Code");
+        }
+
+        public VOUCHER toDB_Voucher()
+        {
+            VOUCHER voucher = new VOUCHER();
+            //voucher.ID = this.Code;
+            voucher.BEGINTIME = this.DateStart;
+            voucher.ENDTIME = this.dateEnd;
+            voucher.PERCENT = this._value;
+            return voucher;
+        }
+
+        public void SaveToDB(VoucherViewModel viewModel)
+        {
+            using (mainEntities db = new mainEntities())
+            {
+                db.VOUCHERs.Add(viewModel.toDB_Voucher());
+            }
+        }
+
+        public static bool HasExisted(String code)
+        {
+            bool rs = false;
+            /*using (mainEntities db = new mainEntities())
+            {
+                VOUCHER voucher = db.VOUCHERs.Where((v) => v.ID == code).First();
+                rs = voucher != null;
+            }*/
+            return rs;
+        }
     }
 }
