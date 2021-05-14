1a8dd5cf14f8168ff3ae61e5c3a9ace885214ba2
Dragnell <bang2001vl@outlook.com.vn>
2021-05-14 15:16:30 +0700

    Finish AddVoucher

--- MainProject/VoucherWorkSpace/VoucherView.xaml.cs
+++ MainProject/VoucherWorkSpace/VoucherView.xaml.cs
@@ -27,17 +27,10 @@ namespace MainProject.VoucherWorkSpace
             this.dateStart.DisplayDateStart = DateTime.Now.Date;
         }
 
-        private void txt_value_LostFocus(object sender, RoutedEventArgs e)
-        {
-            /*TextBox textBox = (TextBox)sender;
-            VoucherViewModel viewModel = (VoucherViewModel)textBox.DataContext;
-            textBox.Text = (viewModel.Value);*/
-        }
-
         private void cbx_auto_Checked(object sender, RoutedEventArgs e)
         {
             VoucherViewModel viewModel = (VoucherViewModel)this.DataContext;
-            viewModel.Code = viewModel.getRandomCode();
+            viewModel.GetAvaiableCode();
             txtCode.IsEnabled = false;
         }
 
