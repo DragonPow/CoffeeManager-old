44f4d2f34cc421a0226e3ebe79a05ae52f094acb
UIT.19520266 <19520266@gm.uit.edu.vn>
2021-05-29 20:52:32 +0700

    [ADD HELPER CLASS] - Helper class for model
    Thêm các hàm và biến static hỗ trợ cho các class sau:
    - POSITION_EMPLOYEE
    - STATUS_TABLE
    - TYPE_PRODUCT

---
+++ MainProject/TYPE_PRODUCT.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainProject.Model
+{
+    public partial class TYPE_PRODUCT
+    {
+        public static ObservableCollection<string> ListType;
+        public static void loadListType()
+        {
+            using (var db = new mainEntities())
+            {
+                ListType = new ObservableCollection<string>(db.TYPE_PRODUCT.Select(i => i.Type).ToList());
+            }
+        }
+    }
+}
