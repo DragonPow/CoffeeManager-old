5b35039fd20d5bc6cf2c00e72caf939bcc9e59ca
Dragnell <bang2001vl@outlook.com.vn>
2021-06-02 18:53:17 +0700

    Almost Done
    Connect to database is okay
    Binding run smoothly

--- MainProject/StatisticWorkSpace/StatisticView.xaml.cs
+++ MainProject/StatisticWorkSpace/StatisticView.xaml.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -23,24 +24,125 @@ namespace MainProject.StatisticWorkSpace
     {
         public StatisticView()
         {
-            InitializeComponent();
             Initialized += StatisticView_Initialized;
+            InitializeComponent();
+            txtDatePicker.CalendarOpened += TxtDatePicker_CalendarOpened;
+            btnSubmit.Click += BtnSubmit_Click;
+        }
+
+        private void TxtDatePicker_CalendarOpened(object sender, RoutedEventArgs e)
+        {
+            if (txtDatePicker.SelectedDate == null) return;
+            var datePicker = (DatePicker)sender;
+            var popup = (Popup)datePicker.Template.FindName("PART_Popup", datePicker);
+            Calendar calendar = ((Calendar)popup.Child);
+            calendar.SetValue(MaterialDesignThemes.Wpf.CalendarAssist.IsHeaderVisibleProperty, false);
+            if (calendar.DisplayMode == CalendarMode.Month)
+            {
+                if (datePicker.SelectedDate != null)
+                {
+                    DateTime date = (DateTime)datePicker.SelectedDate;
+                    calendar.SelectionMode = CalendarSelectionMode.SingleRange;
+                    datePicker.SelectedDate = date;
+                    DateTime minDate, maxDate;
+                    selectWeek(date, out minDate, out maxDate);
+                    calendar.SelectedDates.AddRange(minDate, maxDate);
+                }
+            }
+            Console.WriteLine("Show mine calendar");
         }
 
         private void StatisticView_Initialized(object sender, EventArgs e)
         {
-            StatisticViewModel viewModel = (StatisticViewModel)DataContext;
-            if (viewModel != null)
+            DateTime today = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, 0, 0, 1);
+            cbxStatisticMode_SelectionChanged(cbxStatisticMode, null);
+            txtDatePicker.SelectedDate = today;
+
+        }
+
+        private void BtnSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtDatePicker.SelectedDate != null)
             {
-                //SeriesCollection = 
+                DateTime minDate = DateTime.Now;
+                DateTime maxDate = DateTime.Now;
+                DateTime selectedDate = (DateTime)txtDatePicker.SelectedDate;
+                switch (cbxStatisticMode.SelectedIndex)
+                {
+                    case 0:
+                        selectWeek(selectedDate,out minDate,out maxDate);
+                        break;
+                    case 1:
+                        selectMonth(selectedDate, out minDate, out maxDate);
+                        break;
+                    case 2:
+                        selectYear(selectedDate, out minDate, out maxDate);
+                        break;
+                }
+                Console.WriteLine("Statistic range {0} - {1}", minDate.ToShortDateString(), maxDate.ToShortDateString());
+                StatisticViewModel viewModel = DataContext as StatisticViewModel;
+                if(viewModel != null)
+                {
+                    viewModel.setTimeRange(minDate, maxDate);
+                }
             }
         }
 
-        private void Calendar_SelectedDatesChanged(object sender, SelectionChangedEventArgs e)
+
+        void selectWeek(DateTime date, out DateTime minDate, out DateTime maxDate)
         {
-            Calendar calendar = sender as Calendar;
-            calendar.SelectedDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day);
-            Console.WriteLine("On selected date change");
+            minDate = new DateTime(date.Year, date.Month, date.Day, 0, 0, 0);
+            maxDate = minDate.AddDays(7).AddSeconds(-1);
+
+            //Console.WriteLine(String.Format("Selected Week : {0} - {1}", minDate.ToShortDateString(), maxDate.ToShortDateString()));
         }
+
+        void selectMonth(DateTime date, out DateTime minDate, out DateTime maxDate)
+        {
+            minDate = new DateTime(date.Year, date.Month, 1, 0, 0, 1);
+            maxDate = new DateTime(date.Year, date.Month, DateTime.DaysInMonth(date.Year, date.Month), 23, 59, 59);
+
+            //Console.WriteLine(String.Format("Selected Month : {0} - {1}", minDate.ToShortDateString(), maxDate.ToShortDateString()));
+        }
+
+        void selectYear(DateTime date, out DateTime minDate, out DateTime maxDate)
+        {
+            minDate = maxDate = new DateTime(date.Year, 1, 1, 0, 0, 1);
+            maxDate = new DateTime(date.Year, 12, 31, 23, 59, 59);
+
+            //Console.WriteLine(String.Format("Selected Month : {0} - {1}", minDate.ToShortDateString(), maxDate.ToShortDateString()));
+        }
+
+        string[] mDateFormat_Week = new string[] { "7 ngày từ ", "dd/MM/yyyy", null };
+        public string[] MyDateFormat_Week => mDateFormat_Week;
+        string[] mDateFormat_Month = new string[] { "Tháng ", "MM/yyyy", null };
+        string[] mDateFormat_Year = new string[] { "Năm ", "yyyy", null };
+
+        private void cbxStatisticMode_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBox comboBox = (ComboBox)sender;
+            if (txtDatePicker != null)
+            {
+                switch (comboBox.SelectedIndex)
+                {
+                    case 0:
+                        txtDatePicker.SetValue(DatePickerYearOfDecade.IsYearDecadeProperty, false);
+                        txtDatePicker.SetValue(DatePickerMonthOfYear.IsMonthYearProperty, false);
+                        txtDatePicker.SetValue(DatePickerDateFormat.DateFormatProperty, mDateFormat_Week);
+                        break;
+                    case 1:
+                        txtDatePicker.SetValue(DatePickerYearOfDecade.IsYearDecadeProperty, false);
+                        txtDatePicker.SetValue(DatePickerMonthOfYear.IsMonthYearProperty, true);
+                        txtDatePicker.SetValue(DatePickerDateFormat.DateFormatProperty, mDateFormat_Month);
+                        break;
+                    case 2:
+                        txtDatePicker.SetValue(DatePickerYearOfDecade.IsYearDecadeProperty, true);
+                        txtDatePicker.SetValue(DatePickerMonthOfYear.IsMonthYearProperty, false);
+                        txtDatePicker.SetValue(DatePickerDateFormat.DateFormatProperty, mDateFormat_Year);
+                        break;
+                }
+            }
+        }
+
     }
 }
--- MainProject/StatisticWorkSpace/StatisticViewModel.cs
+++ MainProject/StatisticWorkSpace/StatisticViewModel.cs
@@ -1,4 +1,5 @@
-﻿using MaterialDesignThemes.Wpf;
+﻿using MainProject.Model;
+using MaterialDesignThemes.Wpf;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -24,6 +25,7 @@ namespace MainProject.StatisticWorkSpace
         public ObservableCollection<StatisticModel> ListModel { get => listModel; }
 
         StatisticMode currentMode = StatisticMode.DayOfWeek;
+        public StatisticMode CurrentMode => currentMode;
         public String CurrentMode_String => StatisticEnum.GetString(currentMode);
         public void SetCurrentMode(int index)
         {
@@ -32,6 +34,230 @@ namespace MainProject.StatisticWorkSpace
         }
 
         public Func<double, string> formaterLabelAxisY { get; set; }
+        private String getMoneyLabel(int money)
+        {
+            string[] prefix = new string[] { "k", "tr", "tỷ" };
+            int i = 0;
+            while (money / 1000000 != 0)
+            {
+                money /= 1000; 
+                i++;
+            }
+            double rs = money / 1000d;
+
+            return String.Format("{0:0.#}", rs) + prefix[i];
+        }
+
+        public void setTimeRange(DateTime minDate, DateTime maxDate)
+        {
+            listModel.Clear();
+
+            List<StatisticModel> data = null;
+            switch (CurrentMode)
+            {
+                case StatisticMode.DayOfWeek:
+                    data = statisticByDay(minDate, maxDate);
+                    break;
+                case StatisticMode.WeekOfMonth:
+                    data = statisticByWeek(minDate, maxDate);
+                    break;
+                case StatisticMode.MonthOfYear:
+                    data = statisticByMonth(minDate, maxDate);
+                    break;
+            }
+
+            foreach (var model in data)
+            {
+                ListModel.Add(model);
+            }
+
+            OnPropertyChanged(nameof(ListModel));
+        }
+
+        public List<StatisticModel> statisticByDay(DateTime minDate, DateTime maxDate)
+        {
+            using (mainEntities db = new mainEntities())
+            {
+                var data = db.BILLs.Where(b => b.CheckoutDay >= minDate && b.CheckoutDay <= maxDate)
+                    .Join(db.DETAILBILLs, b => b.ID, dt => dt.ID_Bill,
+                    (b, dt) => new
+                    {
+                        PD_ID = dt.ID_Product,
+                        Date = b.CheckoutDay,
+                        Amount = dt.Amount
+                    }).Join(db.PRODUCTs, r => r.PD_ID, pd => pd.ID,
+                    (r, pd) => new
+                    {
+                        pd.Name,
+                        Revenue = pd.Price * r.Amount,
+                        r.Date
+                    });
+                Dictionary<DateTime, StatisticModel> dictionary = new Dictionary<DateTime, StatisticModel>();
+                foreach (var group in data)
+                {
+                    if (group.Date.HasValue)
+                    {
+                        DateTime date = new DateTime(group.Date.Value.Year, group.Date.Value.Month, group.Date.Value.Day, 0, 0, 0);
+                        if (!dictionary.ContainsKey(date))
+                        {
+                            var model = new StatisticModel();
+                            model.TimeMin = date;
+                            model.TimeMax = date.AddDays(1).AddSeconds(-1);
+                            model.Revenue = group.Revenue.Value;
+                            model.Label = GetLabel(model);
+                            model.Title = GetLabel(model);
+                            dictionary.Add(date, model);
+                        }
+                        else
+                        {
+                            var model = dictionary[date];
+                            model.Revenue += group.Revenue.Value;
+                        }
+                    }
+                }
+                return dictionary.Values.ToList();
+            }
+        }
+
+        public List<StatisticModel> statisticByWeek(DateTime minDate, DateTime maxDate)
+        {
+            using (mainEntities db = new mainEntities())
+            {
+                var data = db.BILLs.Where(b => b.CheckoutDay >= minDate && b.CheckoutDay <= maxDate)
+                    .Join(db.DETAILBILLs, b => b.ID, dt => dt.ID_Bill,
+                    (b, dt) => new
+                    {
+                        PD_ID = dt.ID_Product,
+                        Date = b.CheckoutDay,
+                        Amount = dt.Amount
+                    }).Join(db.PRODUCTs, r => r.PD_ID, pd => pd.ID,
+                    (r, pd) => new
+                    {
+                        pd.Name,
+                        Revenue = pd.Price * r.Amount,
+                        r.Date
+                    });
+                Dictionary<DateTime, StatisticModel> dictionary = new Dictionary<DateTime, StatisticModel>();
+                foreach (var group in data)
+                {
+                    if (group.Date.HasValue)
+                    {
+                        DateTime date = new DateTime(group.Date.Value.Year, group.Date.Value.Month, group.Date.Value.Day, 0, 0, 0);
+                        while (date.DayOfWeek != DayOfWeek.Monday) { date = date.AddDays(-1); }
+                        if (!dictionary.ContainsKey(date))
+                        {
+                            var model = new StatisticModel();
+                            model.TimeMin = date;
+                            model.TimeMax = date.AddDays(7).AddSeconds(-1);
+                            model.Revenue = group.Revenue.Value;
+                            model.Label = GetLabel(model);
+                            model.Title = GetLabel(model);
+                            dictionary.Add(date, model);
+                        }
+                        else
+                        {
+                            var model = dictionary[date];
+                            model.Revenue += group.Revenue.Value;
+                        }
+                    }
+                }
+                return dictionary.Values.ToList();
+            }
+        }
+
+        public List<StatisticModel> statisticByMonth(DateTime minDate, DateTime maxDate)
+        {
+            using (mainEntities db = new mainEntities())
+            {
+                var data = db.BILLs.Where(b => b.CheckoutDay >= minDate && b.CheckoutDay <= maxDate)
+                    .Join(db.DETAILBILLs, b => b.ID, dt => dt.ID_Bill,
+                    (b, dt) => new
+                    {
+                        PD_ID = dt.ID_Product,
+                        Date = b.CheckoutDay,
+                        Amount = dt.Amount
+                    }).Join(db.PRODUCTs, r => r.PD_ID, pd => pd.ID,
+                    (r, pd) => new
+                    {
+                        pd.Name,
+                        Revenue = pd.Price * r.Amount,
+                        r.Date
+                    });
+                Dictionary<DateTime, StatisticModel> dictionary = new Dictionary<DateTime, StatisticModel>();
+                foreach (var group in data)
+                {
+                    if (group.Date.HasValue)
+                    {
+                        DateTime date = new DateTime(group.Date.Value.Year, group.Date.Value.Month, 1, 0, 0, 0);
+                        if (!dictionary.ContainsKey(date))
+                        {
+                            var model = new StatisticModel();
+                            model.TimeMin = date;
+                            model.TimeMax = date.AddMonths(1).AddSeconds(-1);
+                            model.Revenue = group.Revenue.Value;
+                            model.Label = GetLabel(model);
+                            model.Title = GetLabel(model);
+                            dictionary.Add(date, model);
+                        }
+                        else
+                        {
+                            var model = dictionary[date];
+                            model.Revenue += group.Revenue.Value;
+                        }
+                    }
+                }
+                return dictionary.Values.ToList();
+            }
+        }
+
+        public String GetLabel(StatisticModel model)
+        {
+            String rs = "<Error>";
+            switch (CurrentMode)
+            {
+                case StatisticMode.DayOfWeek:
+                    rs = GetDayOfWeek(model.TimeMin.DayOfWeek);
+                    break;
+                case StatisticMode.WeekOfMonth:
+                    rs = String.Format("{0} - {1} năm {2}", model.TimeMin.ToString("dd/MM"),
+                        model.TimeMax.ToString("dd/MM"), model.TimeMin.Year.ToString());
+                    break;
+                case StatisticMode.MonthOfYear:
+                    rs = String.Format("Tháng {0}", model.TimeMin.ToString("MM/yyyy"));
+                    break;
+            }
+            return rs;
+        }
+
+        public String GetDayOfWeek(DayOfWeek dayOfweek)
+        {
+            String rs = null;
+            switch (dayOfweek)
+            {
+                case DayOfWeek.Monday:
+                    rs = "Thứ Hai";
+                    break;
+                case DayOfWeek.Tuesday:
+                    rs = "Thứ Ba";
+                    break;
+                case DayOfWeek.Wednesday:
+                    rs = "Thứ Tư";
+                    break;
+                case DayOfWeek.Thursday:
+                    rs = "Thứ Năm";
+                    break;
+                case DayOfWeek.Friday:
+                    rs = "Thứ Sáu";
+                    break;
+                case DayOfWeek.Saturday:
+                    rs = "Thứ Bảy";
+                    break;
+                case DayOfWeek.Sunday:
+                    rs = "Chủ Nhật";
+                    break;
+            }
+            return rs;
+        }
 
         public StatisticViewModel()
         {
@@ -43,7 +269,7 @@ namespace MainProject.StatisticWorkSpace
                 new StatisticModel(){ Label = "Label4", Revenue = 60000},
                 new StatisticModel(){ Label = "Label5", Revenue = 30000}
             };
-            formaterLabelAxisY = val => (val/1000).ToString("N0")+"k";
+            formaterLabelAxisY = val => getMoneyLabel((int)val);
         }
     }
 }
