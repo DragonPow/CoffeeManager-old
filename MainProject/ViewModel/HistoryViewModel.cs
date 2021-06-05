604dd399094db6753d558bcb601f39c7ab3044c8
UIT.19521973 <67250352+Nhuc-Huynh@users.noreply.github.com>
2021-06-06 00:05:03 +0700

    Update

--- MainProject/ViewModel/HistoryViewModel.cs
+++ MainProject/ViewModel/HistoryViewModel.cs
@@ -17,13 +17,14 @@ namespace MainProject.ViewModel
         private BILL _CurrentBill;
         private int _NumberPage;
         private int _NumberAllpage;
-        public static int Number_Bill_in_Page = 20;
+        public static int Number_Bill_in_Page = 20;      
 
         private DateTime _BeginTime;
         private DateTime _EndTime;
 
         ICommand _Load_Detail_Bill;
         ICommand _Exit_Detail_Bill;
+        ICommand _Search_Bill;
         #endregion
 
         #region Properties
@@ -67,13 +68,9 @@ namespace MainProject.ViewModel
         }
         public int NumberAllPage
         {
-            get 
-            {
-                using ( var db = new mainEntities ())
-                {
-                    return ((db.BILLs.Count()/ Number_Bill_in_Page) + (db.BILLs.Count() % Number_Bill_in_Page != 0? 1 : 0)) ;
-                }    
-            }            
+            get;
+            set;
+                      
         }
         public DateTime BeginTime
         {
@@ -134,12 +131,45 @@ namespace MainProject.ViewModel
 
         public void Load_Detail_Bill()
         {
+           
             BillView view = new BillView();
             view.DataContext = new BillViewModel() { Total = (long)CurrentBill.TotalPrice, CurrentBill = CurrentBill, CodeDiscount = CurrentBill.VOUCHER.CODE, Discount = ( (int)CurrentBill.TotalPrice*CurrentBill.VOUCHER.Percent/ ( 100 - CurrentBill.VOUCHER.Percent)) };
             view.Show();
 
         }
 
+        public ICommand Search_BillCommand
+        {
+            get
+            {
+                if (_Search_Bill == null)
+                    _Search_Bill = new RelayingCommand<object>(a => Search_Bill());
+                return _Search_Bill;
+            }
+        }
+
+        public void Search_Bill()
+        {
+            if (EndTime < BeginTime)
+            {
+                WindowService.Instance.OpenMessageBox("Ngày bắt đầu phải nhỏ hơn hoặc bằng ngày kết thúc!", "Lỗi", System.Windows.MessageBoxImage.Error);
+                return;
+            }
+
+            NumberPage = 1;
+            using ( var db = new mainEntities())
+            {
+                NumberAllPage = ((db.BILLs.Where((b => (b.CheckoutDay >= BeginTime && b.CheckoutDay <= EndTime))).Count() / Number_Bill_in_Page) + (db.BILLs.Count() % Number_Bill_in_Page != 0 ? 1 : 0));
+
+                var list = db.BILLs.Where(b => (b.CheckoutDay >= BeginTime && b.CheckoutDay <= EndTime)).OrderBy(b => b.ID).Take(Number_Bill_in_Page).ToList();
+
+                if (list == null) return;
+
+                ListBill = new ObservableCollection<BILL>(list);
+            }
+           
+
+        }
         public ICommand Exit_Detail_BillCommand
         {
             get
@@ -153,9 +183,12 @@ namespace MainProject.ViewModel
 
         void LoadBillByNumberPage()
         {
+            
             using (var db = new mainEntities())
             {
-               var list = db.BILLs.Where(b => (b.CheckoutDay >= BeginTime && b.CheckoutDay <= EndTime)).OrderBy(b => b.ID).Skip(NumberPage).Take(Number_Bill_in_Page).ToList();
+              
+
+                var list = db.BILLs.Where(b => (b.CheckoutDay >= BeginTime && b.CheckoutDay <= EndTime)).OrderBy(b => b.ID).Skip(NumberPage - 1).Take(Number_Bill_in_Page).ToList();
 
                 if (list == null) return;
 
